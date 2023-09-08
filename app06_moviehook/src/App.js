74c1c313cbb088a8fe1e2dd1af49851df535df71
ssc0831 <ssc0831@naver.com>
2023-09-08 17:30:42 +0900

    React 230908

--- app06_moviehook/src/App.js
+++ app06_moviehook/src/App.js
@@ -1,25 +1,24 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./routers/Home";
-import About from "./routers/About";
-import Navigation from "./components/Navigation";
-import Poster from "./routers/Poster";
-import Detail from "./routers/Detail";
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route  path={"/"} element ={<Home />}> </Route>
-        <Route  path={"/about"} element ={<About />}> </Route>
-        <Route  path={"/poster"} element ={<Poster />}> </Route>
-        <Route  path={"/detail"} element={<Detail />} />
-      </Routes>
-    
-    </BrowserRouter>
-   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
