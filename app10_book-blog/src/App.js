f992c11d3ece961e29d54c004aa88fb5fb606bba
ssc0831 <ssc0831@naver.com>
2023-09-15 16:59:25 +0900

    React

--- app10_book-blog/src/App.js
+++ app10_book-blog/src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 // Pages
 import MainPage from './component/page/MainPage';
